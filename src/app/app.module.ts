401e243eb361adcbd52645a4665728d1a22f9dd9
Parvez Shaikh <parvez.shaikh@gmail.com>
2020-09-27 00:34:43 +0530

    meterial bootstrap setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {NoopAnimationsModule} from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+import { MatButtonModule,MatCardModule } from '@angular/material';
 
 
 import { AppComponent } from './app.component';
+import { MovieListingComponent } from './components/movie-listing/movie-listing.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MovieListingComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    NoopAnimationsModule,
+    MatButtonModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/movie-listing/movie-listing.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-movie-listing',
+  templateUrl: './movie-listing.component.html',
+  styleUrls: ['./movie-listing.component.css']
+})
+export class MovieListingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
